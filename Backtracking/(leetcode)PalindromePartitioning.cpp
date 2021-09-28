8214388984f0e48c8dd7bde3075e04c2d055ad45
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2021-09-28 18:35:20 +0530

    added Palindrome Partitioning

---
+++ Backtracking/(leetcode)PalindromePartitioning.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> pal;
+        int n = s.size();
+        string str = "";
+                
+        palindromePartitions(s, ans, pal, str, n, 0);
+        return ans;
+    }
+    
+    void palindromePartitions(string s, vector<vector<string>> &ans, vector<string> &pal, string &str, int n, int z){
+        //Base Case
+        if(z == n){
+            ans.push_back(pal);
+            return;
+        }
+        
+        //Recursive Case
+        for(int i=z; i < n; i++){
+            str = str + s[i];
+            if(isPalindrome(str)){
+                pal.push_back(str);
+                str = "";
+                palindromePartitions(s, ans, pal, str, n, i+1);
+                
+                //Backtracking
+                str = pal.back();
+                pal.pop_back();
+                cout<<str<<" ";
+            }
+        }
+        return;
+    }
+    
+    bool isPalindrome(string str){
+        int len = str.size();
+        int mid = len/2;
+        
+        if(len == 1){
+            return true;
+        }
+        
+        int i=0;
+        while(i < mid){
+            if(str[i] != str[len-i-1]){
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+};
