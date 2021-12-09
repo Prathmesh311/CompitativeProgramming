c815449648e3f5739989b7caea63a6ac1d22e79a
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2021-12-09 06:27:58 +0530

    levelOrderPrint added

--- Binary Tree/LevelOrderPrint.cpp
+++ Binary Tree/LevelOrderPrint.cpp
@@ -29,19 +29,43 @@ node* buildTree(){
     return root;
 }
 
-void preOrderTraversal(node* root){
+
+int height(node* root){
+    if(root == NULL){
+        return 0;
+    }
+
+    int leftTree = height(root->left);
+    int rightTree = height(root->right);
+
+    return max(leftTree, rightTree) + 1;
+}
+
+void printKthLevel(node* root, int k){
     if(root == NULL){
         return;
     }
 
-    cout<<root->data<<" ";
-    preOrderTraversal(root->left);
-    preOrderTraversal(root->right);
+    if(k == 1){
+        cout<<root->data<<" ";
+    }
+
+    printKthLevel(root->left, k-1);
+    printKthLevel(root->right, k-1);
+}
+
+void levelOrderPrint(node* root){
+    int H = height(root);
+
+    for(int i=1; i <= H; i++){
+        printKthLevel(root, i);
+    }
 }
 
 int main(){
     node* root = buildTree();
-    preOrderTraversal(root);
+
+    levelOrderPrint(root);
 
     return 0;
 }
